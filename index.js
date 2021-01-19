b46a34afddbed792ffc012f2beb9ab904a1d06ad
hannanuddin78 <hannanuddin78@gmail.com>
2021-01-20 02:55:51 +0600

    3rd party img uploade done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const {cloudinary}  = require("./utils/cloudinary");
 const express = require("express");
 require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
@@ -5,16 +6,18 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 
-
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ugsfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+app.use(cors());
 
 const port = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ugsfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -29,10 +32,10 @@ client.connect((err) => {
   console.log("database connect");
 
   app.post("/addEvent", (req, res) => {
-    const event = req.body;
-    eventsCollection.insertMany(event).then((result) => {
+    const event = (req.body);
+    eventsCollection.insertOne(event).then((result) => {
       console.log(result.insertedCount);
-      res.send(result.insertedCount);
+      res.send(result.insertedCount > 0);
     });
   });
 
@@ -76,6 +79,22 @@ client.connect((err) => {
         res.send(result);
       });
   });
+});
+
+app.post("/api/upload", async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "volunter_img",
+      width:180,
+      height:260,
+    });
+    console.log(uploadResponse);
+    res.json({ url: `${uploadResponse.url}` });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
 });
 
 app.get("/", (req, res) => {
